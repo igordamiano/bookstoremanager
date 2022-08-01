cdd823989ee3341070824e41f2c7227c0c749cb8
Igor Damiano <igordamiano@gmail.com>
2022-08-01 20:04:00 -0300

    Primeiro commit: Estrutura do projeto criado

---
+++ src/test/java/com/igor/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
